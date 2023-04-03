226577f3d92fba9bfa9ae027d4db36304d8fafb8
Lalomax <lalomax365@gmail.com>
2023-04-03 14:14:47 -0400

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  FlatList,
+  Image,
+  TouchableWithoutFeedback,
+  Dimensions
+} from "react-native";
+import data from "./src/data";
+console.log(data);
 export default function App() {
+  const renderItem = ({item, index}) => {
+    return (
+      <View style={styles.listContainer}>
+        <View style={styles.imageContainer}>
+          <Image source={item.image} style={styles.image} />
+        </View>
+        <Text style={styles.nameText}>{item.name}</Text>
+        <Text style={styles.priceText}>{item.price}</Text>
+        <TouchableWithoutFeedback onPress={() => {
+          console.log("Buy Now:", index)
+        }}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Buy now!</Text>
+          </View>
+        </TouchableWithoutFeedback>
+      </View>
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        numColumns={2}
+        showsVerticalScrollIndicator={false}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    // alignItems: "center",
+    // justifyContent: "center",
   },
+  listContainer: {
+    width: Dimensions.get('window').width / 2 - 20,
+    backgroundColor: "white",
+    margin: 10,
+    borderRadius: 20,
+  },
+  imageContainer: {
+    margin: 15,
+    borderRadius: 10,
+    overflow: "hidden",
+  },
+  image: {
+    width: "100%",
+    height: undefined,
+    aspectRatio: 1,
+  },
+  nameText: {
+    fontWeight: "bold",
+    marginLeft: 15,
+  },
+  priceText: {
+    color: "orange",
+    fontWeight: "bold",
+    marginLeft: 15,
+    marginTop: 10,
+  },
+  button: {
+    backgroundColor: '#62513E',
+    padding: 10,
+    margin: 15,
+    borderRadius: 10
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center'
+  }
 });
---
+++ src/data.js
@@ -0,0 +1,58 @@
+const data = [
+    {
+      id: 1,
+      name: 'Lorem Ipsum',
+      price: '$100',
+      image: require('../assets/image1.jpeg'),
+    },
+    {
+      id: 2,
+      name: 'Lorem Ipsum',
+      price: '$110',
+      image: require('../assets/image2.jpeg'),
+    },
+    {
+      id: 3,
+      name: 'Lorem Ipsum',
+      price: '$90',
+      image: require('../assets/image3.jpeg'),
+    },
+    {
+      id: 4,
+      name: 'Lorem Ipsum',
+      price: '$95',
+      image: require('../assets/image4.jpeg'),
+    },
+    {
+      id: 5,
+      name: 'Lorem Ipsum',
+      price: '$115',
+      image: require('../assets/image5.jpeg'),
+    },
+    {
+      id: 6,
+      name: 'Lorem Ipsum',
+      price: '$120',
+      image: require('../assets/image6.jpeg'),
+    },
+    {
+      id: 7,
+      name: 'Lorem Ipsum',
+      price: '$100',
+      image: require('../assets/image7.jpeg'),
+    },
+    {
+      id: 8,
+      name: 'Lorem Ipsum',
+      price: '$99',
+      image: require('../assets/image8.jpeg'),
+    },
+    {
+      id: 9,
+      name: 'Lorem Ipsum',
+      price: '$130',
+      image: require('../assets/image9.jpeg'),
+    },
+  ];
+
+  export default data;
